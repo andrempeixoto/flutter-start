48ee67b6ec87cbbdbce10eea11249cb97011e7f3
Andre Peixoto <peixoto.amp@gmail.com>
2021-01-30 00:37:50 -0300

    Styles home

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,59 @@ class _WeatherAppState extends State<WeatherApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Column(
-          children: <Widget>[Container(child: Image.asset('images/dayTime.jpg'))],
+          children: <Widget>[
+            Container(
+              child: Image.asset('images/dayTime.jpg'),
+            ),
+            // SizedBox(height: 30.0), // not using this element as Container margin is used below
+            Container(
+              margin: EdgeInsets.only(top: 30.0),
+              child: Text(
+                'You are in:',
+                style: TextStyle(
+                  fontSize: 35.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue[500],
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    child: Text('Pres. Prudente',
+                        style: TextStyle(
+                          fontSize: 35.0,
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ),
+                  SizedBox(width: 10.0),
+                  Container(
+                    child: Icon(
+                      Icons.location_on,
+                      color: Colors.red,
+                      size: 40.0,
+                    ),
+                  ),
+                  Card(
+                    margin: EdgeInsets.symmetric(vertical: 17.0, horizontal: 25.0),
+                    color: Colors.white,
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.wb_sunny,
+                        color: Colors.amber,
+                      ),
+                      title: Text('Temp: 27 ºC'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
