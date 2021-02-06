e6e81e85c2ec022629ca37098df0b32a02a9672f
Andre Peixoto <peixoto.amp@gmail.com>
2021-02-06 00:07:25 -0300

    Finishes WeatherData.dart

--- lib/WeatherData.dart
+++ lib/WeatherData.dart
@@ -4,4 +4,13 @@ class WeatherData {
   final String temperature;
 
   WeatherData({this.city, this.description, this.temperature});
+
+  // the function below creates one instance of the class that can be used all over
+  factory WeatherData.fromJSON(Map<String, dynamic> json) {
+    return WeatherData(
+      city: json['name'],
+      description: json['weather'][0]['description'],
+      temperature: json['main']['temp'],
+    );
+  }
 }
